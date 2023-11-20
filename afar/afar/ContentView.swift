c38c8a08dc1f662c0038e8e1df0e43e77e2ab1a6
Reda Razza <94404873+rera8642@users.noreply.github.com>
2023-11-20 11:54:41 -0700

    XCode File Setup

---
+++ afar/afar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  afar
+//
+//  Created by Reda Razza on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ afar/afar/afarApp.swift
@@ -0,0 +1,17 @@
+//
+//  afarApp.swift
+//  afar
+//
+//  Created by Reda Razza on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct afarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
